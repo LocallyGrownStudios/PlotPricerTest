00fdea738a2bee311c9dc5e13ef4c538131c61e8
Rkey83 <krame407@umn.edu>
2014-06-22 15:19:50 -0500

    Main functionality

---
+++ CDesPlotTool/CDesPlotTool/windowMain.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CDesPlotTool
+{
+    partial class windowMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFileBrowse = new DevComponents.DotNetBar.ButtonX();
+            this.textboxFileName = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.textboxFileSize = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textboxNumPages = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.itemPanel1 = new DevComponents.DotNetBar.ItemPanel();
+            this.SuspendLayout();
+            // 
+            // buttonFileBrowse
+            // 
+            this.buttonFileBrowse.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonFileBrowse.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonFileBrowse.Location = new System.Drawing.Point(424, 15);
+            this.buttonFileBrowse.Name = "buttonFileBrowse";
+            this.buttonFileBrowse.Size = new System.Drawing.Size(53, 20);
+            this.buttonFileBrowse.Style = DevComponents.DotNetBar.eDotNetBarStyle.OfficeMobile2014;
+            this.buttonFileBrowse.TabIndex = 0;
+            this.buttonFileBrowse.Text = "...";
+            this.buttonFileBrowse.Click += new System.EventHandler(this.buttonFileBrowse_Click);
+            // 
+            // textboxFileName
+            // 
+            this.textboxFileName.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
+            // 
+            // 
+            // 
+            this.textboxFileName.Border.Class = "TextBoxBorder";
+            this.textboxFileName.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textboxFileName.Location = new System.Drawing.Point(12, 15);
+            this.textboxFileName.Name = "textboxFileName";
+            this.textboxFileName.PreventEnterBeep = true;
+            this.textboxFileName.Size = new System.Drawing.Size(300, 20);
+            this.textboxFileName.TabIndex = 1;
+            this.textboxFileName.TextChanged += new System.EventHandler(this.textboxFileName_TextChanged);
+            // 
+            // textboxFileSize
+            // 
+            this.textboxFileSize.AccessibleRole = System.Windows.Forms.AccessibleRole.StaticText;
+            // 
+            // 
+            // 
+            this.textboxFileSize.Border.Class = "TextBoxBorder";
+            this.textboxFileSize.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textboxFileSize.Location = new System.Drawing.Point(318, 15);
+            this.textboxFileSize.Name = "textboxFileSize";
+            this.textboxFileSize.PreventEnterBeep = true;
+            this.textboxFileSize.Size = new System.Drawing.Size(56, 20);
+            this.textboxFileSize.TabIndex = 2;
+            this.textboxFileSize.TextChanged += new System.EventHandler(this.textboxFileSize_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textboxNumPages
+            // 
+            this.textboxNumPages.AccessibleRole = System.Windows.Forms.AccessibleRole.StaticText;
+            // 
+            // 
+            // 
+            this.textboxNumPages.Border.Class = "TextBoxBorder";
+            this.textboxNumPages.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textboxNumPages.Location = new System.Drawing.Point(380, 15);
+            this.textboxNumPages.Name = "textboxNumPages";
+            this.textboxNumPages.PreventEnterBeep = true;
+            this.textboxNumPages.Size = new System.Drawing.Size(38, 20);
+            this.textboxNumPages.TabIndex = 3;
+            this.textboxNumPages.TextChanged += new System.EventHandler(this.textboxNumPages_TextChanged);
+            // 
+            // itemPanel1
+            // 
+            // 
+            // 
+            // 
+            this.itemPanel1.BackgroundStyle.Class = "ItemPanel";
+            this.itemPanel1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.itemPanel1.ContainerControlProcessDialogKey = true;
+            this.itemPanel1.DragDropSupport = true;
+            this.itemPanel1.LayoutOrientation = DevComponents.DotNetBar.eOrientation.Vertical;
+            this.itemPanel1.Location = new System.Drawing.Point(12, 41);
+            this.itemPanel1.Name = "itemPanel1";
+            this.itemPanel1.Size = new System.Drawing.Size(465, 658);
+            this.itemPanel1.TabIndex = 4;
+            this.itemPanel1.Text = "itemPanel1";
+            // 
+            // windowMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 711);
+            this.Controls.Add(this.itemPanel1);
+            this.Controls.Add(this.textboxNumPages);
+            this.Controls.Add(this.textboxFileSize);
+            this.Controls.Add(this.textboxFileName);
+            this.Controls.Add(this.buttonFileBrowse);
+            this.Name = "windowMain";
+            this.Text = "windowMain";
+            this.Load += new System.EventHandler(this.windowMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.ButtonX buttonFileBrowse;
+        private DevComponents.DotNetBar.Controls.TextBoxX textboxFileName;
+        private DevComponents.DotNetBar.Controls.TextBoxX textboxFileSize;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textboxNumPages;
+        private DevComponents.DotNetBar.ItemPanel itemPanel1;
+    }
+}
---
+++ CDesPlotTool/CDesPlotTool/windowMain.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.pdfclown.files;
+using org.pdfclown.objects;
+using org.pdfclown.tools;
+using org.pdfclown.documents;
+using org.pdfclown.documents.contents;
+using org.pdfclown.documents.contents.objects;
+using org.pdfclown.documents.interaction;
+using org.pdfclown.documents.interchange.metadata;
+using org.pdfclown.documents.interaction.viewer;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using io = System.IO;
+using files = org.pdfclown.files;
+using bytes = org.pdfclown.bytes;
+
+namespace CDesPlotTool
+{
+    public partial class windowMain : Form
+    {
+        public windowMain()
+        {
+            InitializeComponent();
+        }
+
+        private void windowMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textboxFileName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textboxFileSize_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textboxNumPages_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonFileBrowse_Click(object sender, EventArgs e)
+        {
+            itemPanel1.Controls.Clear();
+            textboxFileName.Clear();
+            textboxFileSize.Clear();
+            textboxNumPages.Clear();
+            openFileDialog1.InitialDirectory = "c:\\";                                                                // Sets the default location for file browser to start in
+            openFileDialog1.Filter = "PDF Files (*.pdf)|*.pdf|All files (*.*)|*.*";                                   // Sets the filter of selectable files
+            openFileDialog1.FilterIndex = 1;                                                                          // Sets the default file type to be selected
+            openFileDialog1.RestoreDirectory = true;                                                                  // Sets wheter or not default directory is restored each time
+            DialogResult result = openFileDialog1.ShowDialog();
+            const int postScriptPoints = 72;
+            int size = -1;                                                                                            // Assign size as an integer
+            double sizeKB = -1.0;                                                                                     // Assign sizeKB as a double
+            double sizeMB = -1.0;                                                                                     // Assign sizeMB as a double
+            double sizeGB = -1.0;                                                                                     // Assign sizeGB as a double
+            string file = openFileDialog1.FileName;
+
+            try
+            {
+                string text = System.IO.File.ReadAllText(file);                                                                 // Assigns text to read selected file legnth
+                size = text.Length;                                                                                   // Assigns size
+                sizeKB = text.Length / 1024;                                                                          // Calculate KB
+                sizeMB = sizeKB / 1024;                                                                               // Calculate MB
+                sizeGB = sizeMB / 1024;                                                                               // Calculate GB
+                sizeKB = Math.Round(sizeKB, 2);                                                                       // Round sizeKB to 2 decimal places
+                sizeGB = Math.Round(sizeGB, 2);                                                                       // Round sizeGB to 2 decimal places
+                sizeMB = Math.Round(sizeMB, 2);                                                                       // Round sizeMB to 2 decimal places
+
+            }
+            catch (System.IO.IOException)                                                                                        // Catch exceptions
+            {
+            }
+            string sourceFilePath = openFileDialog1.FileName;
+            string currentFilePath = openFileDialog1.FileName;
+            using (File currentFile = new File(openFileDialog1.FileName))
+            using (File sourceFile = new File(sourceFilePath))
+            {
+                PdfReader reader = new PdfReader(openFileDialog1.FileName);
+                iTextSharp.text.Rectangle mediabox = reader.GetPageSize(1);
+                Pages sourcePages = sourceFile.Document.Pages;
+                int sourcePagesCount = sourcePages.Count;
+                org.pdfclown.documents.Document documentName = currentFile.Document;
+                Pages documentPages = documentName.Pages;
+                int documentPageCount = documentPages.Count;
+                HashSet<PdfReference> visitedReferences = new HashSet<PdfReference>();
+                long incrementalDataSize = 0;
+                foreach (Page page in documentPages)
+                {
+                    long pageFullDataSize = PageManager.GetSize(page);
+                    long pageDifferentialDataSize = PageManager.GetSize(page, visitedReferences);
+                    incrementalDataSize += pageDifferentialDataSize;
+                    TextBox pageSizes = new TextBox();
+                    pageSizes.Name = "pageSize_" + this.Controls.Count + 1;
+                    pageSizes.Location = new System.Drawing.Point(10, 25 * itemPanel1.Controls.Count);
+                    pageSizes.Text += mediabox.Height / postScriptPoints;
+                    pageSizes.Text += " x ";
+                    pageSizes.Text += mediabox.Width / postScriptPoints;
+                    itemPanel1.Controls.Add(pageSizes);
+                }
+                textboxFileName.Text += file;
+                textboxNumPages.Text += sourcePages.Count;
+            }
+            if (sizeMB >= 1)                                                                                            // If size is larger than or equal to 1 KB
+            {
+                textboxFileSize.Text += sizeMB;                                                                              // Write size as MB to textBoxX2
+                textboxFileSize.Text += " MB";                                                                                // Write MB after size to textBoxX2
+            }
+            else if (sizeGB >= 1)                                                                                       // Else if size is  larger tgab or equal to 1 GB
+            {
+                textboxFileSize.Text += sizeGB;                                                                               // Write size as GB to textBoxX2  
+                textboxFileSize.Text += " GB";                                                                                // Write GB after size to textBoxX2
+            }
+            else
+            {
+                textboxFileSize.Text += size / 1024;                                                                           // Else write size as KB to textBoxX2
+                textboxFileSize.Text += " KB";                                                                                 // Write KB after size to textBoxX2   
+            }
+        }
+
+    }
+}
