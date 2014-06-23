676ef5fc17c7c080dcb01a52cc8790e9eadcf5ee
Rkey83 <krame407@umn.edu>
2014-06-22 19:08:47 -0500

    radial buttons and cost added

--- CDesPlotTool/CDesPlotTool/windowMain.Designer.cs
+++ CDesPlotTool/CDesPlotTool/windowMain.Designer.cs
@@ -28,12 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.buttonFileBrowse = new DevComponents.DotNetBar.ButtonX();
             this.textboxFileName = new DevComponents.DotNetBar.Controls.TextBoxX();
             this.textboxFileSize = new DevComponents.DotNetBar.Controls.TextBoxX();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.textboxNumPages = new DevComponents.DotNetBar.Controls.TextBoxX();
             this.itemPanel1 = new DevComponents.DotNetBar.ItemPanel();
+            this.paperSelectSatin = new System.Windows.Forms.RadioButton();
+            this.paperSelectMatte = new System.Windows.Forms.RadioButton();
+            this.paperSelectBond = new System.Windows.Forms.RadioButton();
+            this.styleManager1 = new DevComponents.DotNetBar.StyleManager(this.components);
             this.SuspendLayout();
             // 
             // buttonFileBrowse
@@ -56,6 +61,7 @@
             // 
             this.textboxFileName.Border.Class = "TextBoxBorder";
             this.textboxFileName.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textboxFileName.Enabled = false;
             this.textboxFileName.Location = new System.Drawing.Point(12, 15);
             this.textboxFileName.Name = "textboxFileName";
             this.textboxFileName.PreventEnterBeep = true;
@@ -71,9 +77,12 @@
             // 
             this.textboxFileSize.Border.Class = "TextBoxBorder";
             this.textboxFileSize.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textboxFileSize.Cursor = System.Windows.Forms.Cursors.No;
+            this.textboxFileSize.Enabled = false;
             this.textboxFileSize.Location = new System.Drawing.Point(318, 15);
             this.textboxFileSize.Name = "textboxFileSize";
             this.textboxFileSize.PreventEnterBeep = true;
+            this.textboxFileSize.ReadOnly = true;
             this.textboxFileSize.Size = new System.Drawing.Size(56, 20);
             this.textboxFileSize.TabIndex = 2;
             this.textboxFileSize.TextChanged += new System.EventHandler(this.textboxFileSize_TextChanged);
@@ -90,6 +99,7 @@
             // 
             this.textboxNumPages.Border.Class = "TextBoxBorder";
             this.textboxNumPages.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textboxNumPages.Enabled = false;
             this.textboxNumPages.Location = new System.Drawing.Point(380, 15);
             this.textboxNumPages.Name = "textboxNumPages";
             this.textboxNumPages.PreventEnterBeep = true;
@@ -99,6 +109,7 @@
             // 
             // itemPanel1
             // 
+            this.itemPanel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             // 
             // 
             // 
@@ -109,17 +120,61 @@
             this.itemPanel1.LayoutOrientation = DevComponents.DotNetBar.eOrientation.Vertical;
             this.itemPanel1.Location = new System.Drawing.Point(12, 41);
             this.itemPanel1.Name = "itemPanel1";
-            this.itemPanel1.Size = new System.Drawing.Size(465, 658);
+            this.itemPanel1.Size = new System.Drawing.Size(406, 658);
             this.itemPanel1.TabIndex = 4;
             this.itemPanel1.Text = "itemPanel1";
+            // 
+            // paperSelectSatin
+            // 
+            this.paperSelectSatin.AutoSize = true;
+            this.paperSelectSatin.Location = new System.Drawing.Point(425, 102);
+            this.paperSelectSatin.Name = "paperSelectSatin";
+            this.paperSelectSatin.Size = new System.Drawing.Size(52, 17);
+            this.paperSelectSatin.TabIndex = 2;
+            this.paperSelectSatin.TabStop = true;
+            this.paperSelectSatin.Text = "Satin ";
+            this.paperSelectSatin.UseVisualStyleBackColor = true;
+            this.paperSelectSatin.CheckedChanged += new System.EventHandler(this.paperSelectSatin_CheckedChanged);
+            // 
+            // paperSelectMatte
+            // 
+            this.paperSelectMatte.AutoSize = true;
+            this.paperSelectMatte.Location = new System.Drawing.Point(425, 79);
+            this.paperSelectMatte.Name = "paperSelectMatte";
+            this.paperSelectMatte.Size = new System.Drawing.Size(52, 17);
+            this.paperSelectMatte.TabIndex = 1;
+            this.paperSelectMatte.TabStop = true;
+            this.paperSelectMatte.Text = "Matte";
+            this.paperSelectMatte.UseVisualStyleBackColor = true;
+            this.paperSelectMatte.CheckedChanged += new System.EventHandler(this.paperSelectMatte_CheckedChanged);
+            // 
+            // paperSelectBond
+            // 
+            this.paperSelectBond.AutoSize = true;
+            this.paperSelectBond.Location = new System.Drawing.Point(424, 56);
+            this.paperSelectBond.Name = "paperSelectBond";
+            this.paperSelectBond.Size = new System.Drawing.Size(50, 17);
+            this.paperSelectBond.TabIndex = 0;
+            this.paperSelectBond.TabStop = true;
+            this.paperSelectBond.Text = "Bond";
+            this.paperSelectBond.UseVisualStyleBackColor = true;
+            this.paperSelectBond.CheckedChanged += new System.EventHandler(this.paperSelectBond_CheckedChanged);
+            // 
+            // styleManager1
+            // 
+            this.styleManager1.ManagerStyle = DevComponents.DotNetBar.eStyle.Office2010Black;
+            this.styleManager1.MetroColorParameters = new DevComponents.DotNetBar.Metro.ColorTables.MetroColorGeneratorParameters(System.Drawing.Color.White, System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(87)))), ((int)(((byte)(154))))));
             // 
             // windowMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(489, 711);
+            this.Controls.Add(this.paperSelectSatin);
             this.Controls.Add(this.itemPanel1);
+            this.Controls.Add(this.paperSelectMatte);
             this.Controls.Add(this.textboxNumPages);
+            this.Controls.Add(this.paperSelectBond);
             this.Controls.Add(this.textboxFileSize);
             this.Controls.Add(this.textboxFileName);
             this.Controls.Add(this.buttonFileBrowse);
@@ -127,6 +182,7 @@
             this.Text = "windowMain";
             this.Load += new System.EventHandler(this.windowMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -134,9 +190,13 @@
 
         private DevComponents.DotNetBar.ButtonX buttonFileBrowse;
         private DevComponents.DotNetBar.Controls.TextBoxX textboxFileName;
-        private DevComponents.DotNetBar.Controls.TextBoxX textboxFileSize;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private DevComponents.DotNetBar.Controls.TextBoxX textboxNumPages;
         private DevComponents.DotNetBar.ItemPanel itemPanel1;
+        public DevComponents.DotNetBar.Controls.TextBoxX textboxFileSize;
+        private DevComponents.DotNetBar.StyleManager styleManager1;
+        private System.Windows.Forms.RadioButton paperSelectSatin;
+        private System.Windows.Forms.RadioButton paperSelectMatte;
+        private System.Windows.Forms.RadioButton paperSelectBond;
     }
 }
--- CDesPlotTool/CDesPlotTool/windowMain.cs
+++ CDesPlotTool/CDesPlotTool/windowMain.cs
@@ -58,50 +58,32 @@ namespace CDesPlotTool
 
         private void buttonFileBrowse_Click(object sender, EventArgs e)
         {
-            itemPanel1.Controls.Clear();
-            textboxFileName.Clear();
-            textboxFileSize.Clear();
-            textboxNumPages.Clear();
+            itemPanel1.Controls.Clear();                                                                              // Clears everything in itemPanel1
+            textboxFileName.Clear();                                                                                  // Clears content of textboxFileName
+            textboxFileSize.Clear();                                                                                  // Clears content of textboxFileSize
+            textboxNumPages.Clear();                                                                                  // Clears content of textboxNumPages
             openFileDialog1.InitialDirectory = "c:\\";                                                                // Sets the default location for file browser to start in
             openFileDialog1.Filter = "PDF Files (*.pdf)|*.pdf|All files (*.*)|*.*";                                   // Sets the filter of selectable files
             openFileDialog1.FilterIndex = 1;                                                                          // Sets the default file type to be selected
             openFileDialog1.RestoreDirectory = true;                                                                  // Sets wheter or not default directory is restored each time
-            DialogResult result = openFileDialog1.ShowDialog();
-            const int postScriptPoints = 72;
+            const int postScriptPoints = 72;                                                                          // Assign postScriptPoints as a constant int
             int size = -1;                                                                                            // Assign size as an integer
             double sizeKB = -1.0;                                                                                     // Assign sizeKB as a double
             double sizeMB = -1.0;                                                                                     // Assign sizeMB as a double
             double sizeGB = -1.0;                                                                                     // Assign sizeGB as a double
+            DialogResult result = openFileDialog1.ShowDialog();                                                       // Opens file explore window
             string file = openFileDialog1.FileName;
-
-            try
+            using (File currentFile = new File(openFileDialog1.FileName))                                             // Using selected file as current file      
+            using (File sourceFile = new File(openFileDialog1.FileName))                                              // Using selected file as source file
             {
-                string text = System.IO.File.ReadAllText(file);                                                                 // Assigns text to read selected file legnth
-                size = text.Length;                                                                                   // Assigns size
-                sizeKB = text.Length / 1024;                                                                          // Calculate KB
-                sizeMB = sizeKB / 1024;                                                                               // Calculate MB
-                sizeGB = sizeMB / 1024;                                                                               // Calculate GB
-                sizeKB = Math.Round(sizeKB, 2);                                                                       // Round sizeKB to 2 decimal places
-                sizeGB = Math.Round(sizeGB, 2);                                                                       // Round sizeGB to 2 decimal places
-                sizeMB = Math.Round(sizeMB, 2);                                                                       // Round sizeMB to 2 decimal places
-
-            }
-            catch (System.IO.IOException)                                                                                        // Catch exceptions
-            {
-            }
-            string sourceFilePath = openFileDialog1.FileName;
-            string currentFilePath = openFileDialog1.FileName;
-            using (File currentFile = new File(openFileDialog1.FileName))
-            using (File sourceFile = new File(sourceFilePath))
-            {
+                HashSet<PdfReference> visitedReferences = new HashSet<PdfReference>();
                 PdfReader reader = new PdfReader(openFileDialog1.FileName);
                 iTextSharp.text.Rectangle mediabox = reader.GetPageSize(1);
-                Pages sourcePages = sourceFile.Document.Pages;
-                int sourcePagesCount = sourcePages.Count;
                 org.pdfclown.documents.Document documentName = currentFile.Document;
+                Pages sourcePages = sourceFile.Document.Pages;
                 Pages documentPages = documentName.Pages;
+                int sourcePagesCount = sourcePages.Count;
                 int documentPageCount = documentPages.Count;
-                HashSet<PdfReference> visitedReferences = new HashSet<PdfReference>();
                 long incrementalDataSize = 0;
                 foreach (Page page in documentPages)
                 {
@@ -109,32 +91,118 @@ namespace CDesPlotTool
                     long pageDifferentialDataSize = PageManager.GetSize(page, visitedReferences);
                     incrementalDataSize += pageDifferentialDataSize;
                     TextBox pageSizes = new TextBox();
-                    pageSizes.Name = "pageSize_" + this.Controls.Count + 1;
-                    pageSizes.Location = new System.Drawing.Point(10, 25 * itemPanel1.Controls.Count);
-                    pageSizes.Text += mediabox.Height / postScriptPoints;
-                    pageSizes.Text += " x ";
-                    pageSizes.Text += mediabox.Width / postScriptPoints;
-                    itemPanel1.Controls.Add(pageSizes);
+                    pageSizes.Enabled = false;
+                    pageSizes.Name = "pageCost_" + this.Controls.Count + 1;
+                    pageSizes.Location = new System.Drawing.Point(25, 10 + (25 * itemPanel1.Controls.Count));
+                    TextBox pageCost = new TextBox();
+                    pageCost.Enabled = false;
+                    pageCost.Name = "pageSize_" + this.Controls.Count + 1;
+                    pageCost.Location = new System.Drawing.Point(150, 10 + (25 * itemPanel1.Controls.Count));
+                    if (mediabox.Height / postScriptPoints <= 42)
+                    {
+                        if (mediabox.Height / postScriptPoints < mediabox.Width / postScriptPoints)
+                        {
+                            pageSizes.Text += mediabox.Height / postScriptPoints;
+                            pageSizes.Text += " x ";
+                            pageSizes.Text += mediabox.Width / postScriptPoints;
+                            if (paperSelectBond.Checked)
+                            {
+                                pageCost.Text += (mediabox.Height / postScriptPoints) * 1/12;
+                            }
+                            else if (paperSelectMatte.Checked)
+                            {
+                                pageCost.Text += mediabox.Height / postScriptPoints * 1/4;
+                            }
+                            else if (paperSelectSatin.Checked)
+                            {
+                                pageCost.Text += mediabox.Height / postScriptPoints * 1/4;
+                            }
+                            else
+                            {
+                                pageCost.Text += "Please choose paper type";
+                            }
+                            itemPanel1.Controls.Add(pageSizes);
+                            itemPanel1.Controls.Add(pageCost);
+                        }
+                    }
+                    else
+                    {
+                        pageSizes.Text += mediabox.Width / postScriptPoints;
+                        pageSizes.Text += " x ";
+                        pageSizes.Text += mediabox.Height / postScriptPoints;
+                        if (paperSelectBond.Checked)
+                        {
+                            pageCost.Text += mediabox.Height / postScriptPoints * 1/12;
+                        }
+                        else if (paperSelectMatte.Checked)
+                        {
+                            pageCost.Text += mediabox.Height / postScriptPoints * 1/4;
+                        }
+                        else if (paperSelectSatin.Checked)
+                        {
+                            pageCost.Text += mediabox.Height / postScriptPoints * 1/4;
+                        }
+                        else
+                        {
+                            pageCost.Text += "Please choose paper type";
+                        }
+                        itemPanel1.Controls.Add(pageSizes);
+                        itemPanel1.Controls.Add(pageCost);
+                    }
                 }
-                textboxFileName.Text += file;
-                textboxNumPages.Text += sourcePages.Count;
+
+                try
+                {
+
+                    string text = System.IO.File.ReadAllText(file);                                                        // Assigns text to read selected file legnth
+                    size = text.Length;                                                                                    // Assigns size
+                    sizeKB = text.Length / 1024;                                                                           // Calculate KB
+                    sizeMB = sizeKB / 1024;                                                                                // Calculate MB
+                    sizeGB = sizeMB / 1024;                                                                                // Calculate GB
+                    sizeKB = Math.Round(sizeKB, 2);                                                                        // Round sizeKB to 2 decimal places
+                    sizeGB = Math.Round(sizeGB, 2);                                                                        // Round sizeGB to 2 decimal places
+                    sizeMB = Math.Round(sizeMB, 2);                                                                        // Round sizeMB to 2 decimal places
+
+                    if (sizeMB >= 1)                                                                                       // If size is larger than or equal to 1 KB
+                    {
+                        textboxFileSize.Text += sizeMB;                                                                    // Write size as MB to textboxFileSize
+                        textboxFileSize.Text += " MB";                                                                     // Write MB after size to textboxFileSize
+                    }
+                    else if (sizeGB >= 1)                                                                                  // Else if size is  larger tgab or equal to 1 GB
+                    {
+                        textboxFileSize.Text += sizeGB;                                                                    // Write size as GB to textboxFileSize  
+                        textboxFileSize.Text += " GB";                                                                     // Write GB after size to textboxFileSize
+                    }
+                    else
+                    {
+                        textboxFileSize.Text += size / 1024;                                                               // Else write size as KB to textboxFileSize
+                        textboxFileSize.Text += " KB";                                                                     // Write KB after size to textboxFileSize
+                    }
+                    textboxFileName.Text += file;                                                                          // Write file path and name to textboxFileName
+                    textboxNumPages.Text += sourcePages.Count;
+                }
+                catch (System.IO.IOException)
+                {
+
+                }
             }
-            if (sizeMB >= 1)                                                                                            // If size is larger than or equal to 1 KB
-            {
-                textboxFileSize.Text += sizeMB;                                                                              // Write size as MB to textBoxX2
-                textboxFileSize.Text += " MB";                                                                                // Write MB after size to textBoxX2
-            }
-            else if (sizeGB >= 1)                                                                                       // Else if size is  larger tgab or equal to 1 GB
-            {
-                textboxFileSize.Text += sizeGB;                                                                               // Write size as GB to textBoxX2  
-                textboxFileSize.Text += " GB";                                                                                // Write GB after size to textBoxX2
-            }
-            else
-            {
-                textboxFileSize.Text += size / 1024;                                                                           // Else write size as KB to textBoxX2
-                textboxFileSize.Text += " KB";                                                                                 // Write KB after size to textBoxX2   
-            }
+
         }
 
+        
+        public void paperSelectBond_CheckedChanged(object sender, EventArgs e)
+        {  
+            // update price
+        }
+
+        public void paperSelectMatte_CheckedChanged(object sender, EventArgs e)
+        {
+            // update price
+        }
+
+        public void paperSelectSatin_CheckedChanged(object sender, EventArgs e)
+        {
+            // update price
+        }
     }
 }
